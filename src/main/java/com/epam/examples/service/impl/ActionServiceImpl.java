6b36b61024797d37f129a843138476a036ff88b9
Ivanko Verigo <verigoivan00@mail.ru>
2019-12-09 14:14:46 +0300

    fix bug with lock

--- src/main/java/com/epam/examples/service/impl/ActionServiceImpl.java
+++ src/main/java/com/epam/examples/service/impl/ActionServiceImpl.java
@@ -5,21 +5,24 @@ import com.epam.examples.service.ActionService;
 import com.epam.examples.service.ServiceException;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+
 public class ActionServiceImpl implements ActionService {
-    private ReentrantLock lock =new ReentrantLock();
+    private final Lock lock = new ReentrantLock();
+
     @Override
     public void buyDollars(Person person, double amount, double coefficient) throws InterruptedException, ServiceException {
         System.out.println(person + "\t WANT to buy DOLLARS");
         TimeUnit.SECONDS.sleep(1);
         lock.lock();
-            double belCash = person.getBelCash();
-            double dollars = person.getDollars();
-            isEnough(person, belCash, -amount * coefficient);
+        double belCash = person.getBelCash();
+        double dollars = person.getDollars();
+        isEnough(person, belCash, -amount * coefficient);
 
-            person.setBelCash(belCash - amount * coefficient);
-            person.setDollars(dollars + amount);
+        person.setBelCash(belCash - amount * coefficient);
+        person.setDollars(dollars + amount);
         lock.unlock();
         System.out.println(person + "\t STOP to buy DOLLARS");
     }
@@ -29,12 +32,12 @@ public class ActionServiceImpl implements ActionService {
         System.out.println(person + "\t WANT to buy EURO");
         TimeUnit.SECONDS.sleep(1);
         lock.lock();
-            double belCash = person.getBelCash();
-            double euro = person.getEuro();
-            isEnough(person, belCash, -amount * coefficient);
+        double belCash = person.getBelCash();
+        double euro = person.getEuro();
+        isEnough(person, belCash, -amount * coefficient);
 
-            person.setBelCash(belCash - amount * coefficient);
-            person.setEuro(euro + amount);
+        person.setBelCash(belCash - amount * coefficient);
+        person.setEuro(euro + amount);
         lock.unlock();
         System.out.println(person + "\t STOP to buy EURO");
     }
@@ -44,12 +47,12 @@ public class ActionServiceImpl implements ActionService {
         System.out.println(person + "\t WANT to sell DOLLARS");
         TimeUnit.SECONDS.sleep(1);
         lock.lock();
-            double belCash = person.getBelCash();
-            double dollars = person.getDollars();
-            isEnough(person, dollars, -amount);
+        double belCash = person.getBelCash();
+        double dollars = person.getDollars();
+        isEnough(person, dollars, -amount);
 
-            person.setDollars(dollars - amount);
-            person.setBelCash(belCash + amount * coefficient);
+        person.setDollars(dollars - amount);
+        person.setBelCash(belCash + amount * coefficient);
         lock.unlock();
         System.out.println(person + "\t STOP to sell DOLLARS");
     }
@@ -59,19 +62,19 @@ public class ActionServiceImpl implements ActionService {
         System.out.println(person + "\t WANT to sell EURO");
         TimeUnit.SECONDS.sleep(1);
         lock.lock();
-            double belCash = person.getBelCash();
-            double euro = person.getEuro();
-            isEnough(person, euro, -amount);
+        double belCash = person.getBelCash();
+        double euro = person.getEuro();
+        isEnough(person, euro, -amount);
 
-            person.setEuro(euro - amount);
-            person.setBelCash(belCash + amount * coefficient);
+        person.setEuro(euro - amount);
+        person.setBelCash(belCash + amount * coefficient);
         lock.unlock();
         System.out.println(person + "\t STOP to sell EURO");
     }
 
-    private void isEnough(Person person, double currency, double value) throws ServiceException {
+    private void isEnough(Person person, double currency, double value)  {
         if (currency + value < 0) {
-            throw new ServiceException(person + "\tDEAL has been denied");
+            System.out.println(person + "\tDEAL has been denied");
         }
     }
 }
